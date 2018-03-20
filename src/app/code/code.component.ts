6fc914c2a098b7d77e7f5f6cdead7d60874e418b
Rane90 <raneisn@gmail.com>
2018-03-20 20:50:16 +0200

    only ran 20/3

--- src/app/code/code.component.ts
+++ src/app/code/code.component.ts
@@ -9,18 +9,19 @@ import {Domain, OrmService, Requirement, User} from '../shared/orm.service';
 })
 export class CodeComponent implements OnInit {
   @Input() user: User;
-  constructor(private orm: OrmService, private router: Router) { }
+  constructor(
+    // private orm: OrmService,
+    private router: Router) { }
 
   ngOnInit() {
-    this.user = this.orm.user;
-    this.orm.domainChanged.subscribe(function(domain) {
-      this.domain = domain;
-      this.requirements = Object.values(this.domain.requirements);
-    });
+    // this.user = this.orm.user;
+    // this.orm.domainChanged.subscribe(function(domain) {
+    //   this.domain = domain;
+    //   this.requirements = Object.values(this.domain.requirements);
+    // });
   }
 
   onNext() {
-    this.orm.setUserCode(this.)
     this.router.navigate(['/annotate/0']);
   }
 }
