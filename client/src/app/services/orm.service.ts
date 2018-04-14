01a0bca2da98f052765872d8ccef5ccc71f6cb6b
achiya elyasaf <10044875+eggsterino@users.noreply.github.com>
2018-04-14 23:06:47 +0000

    fix client compilation errors

--- client/src/app/services/orm.service.ts
+++ client/src/app/services/orm.service.ts
@@ -3,7 +3,6 @@ import {isNewline} from 'codelyzer/angular/styles/cssLexer';
 import { Observable } from 'rxjs/Rx';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import {Response} from '@angular/http';
-import { Injectable } from '@angular/core';
 
 import 'rxjs/add/operator/map';
 
