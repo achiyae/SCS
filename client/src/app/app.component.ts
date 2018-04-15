e224f1dde6580f84fbe462e56b421ef840648a72
achiya elyasaf <10044875+eggsterino@users.noreply.github.com>
2018-04-15 10:16:04 +0000

    a

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import {OrmService} from './services/orm.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [OrmService]
 })
 export class AppComponent {
-  title = 'app';
+  constructor(private db: OrmService) { }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,21 +1,62 @@
-import { FormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+//import {OrmService} from './services/orm.service';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule, Routes} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
+import { InstructionsComponent } from './instructions/instructions.component';
+import { DomainComponent } from './domain/domain.component';
+import { CodeComponent } from './code/code.component';
+import { AnnotateComponent } from './annotate/annotate.component';
+import {DropdownDirective} from './shared/dropdown.directive';
+import { AnnotationsComponent } from './header/annotations/annotations.component';
+import { CodeAnnotationComponent } from './annotate/code-annotation/code-annotation.component';
+import { RequirementComponent } from './annotate/requirement/requirement.component';
+import { RequirementsComponent } from './domain/requirements/requirements.component';
+import { DoneComponent } from './done/done.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'instructions', component: InstructionsComponent },
+  { path: 'domain', component: DomainComponent },
+  { path: 'code', component: CodeComponent },
+  { path: 'annotate', component: AnnotateComponent, children: [
+    {path: ':id', component: CodeAnnotationComponent},
+   ] },
+  { path: 'user', component: UserComponent },
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    HomeComponent,
+    HeaderComponent,
+    InstructionsComponent,
+    DomainComponent,
+    CodeComponent,
+    AnnotateComponent,
+    DropdownDirective,
+    AnnotationsComponent,
+    CodeAnnotationComponent,
+    RequirementComponent,
+    RequirementsComponent,
+    DoneComponent,
+    SidebarComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    NgbModule.forRoot()
+    HttpClientModule,
+    NgbModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
