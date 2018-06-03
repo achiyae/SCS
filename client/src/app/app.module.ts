bc26f2a6b27848c8dd1aeee98bb237ce6e5c1e8b
achiya elyasaf <10044875+eggsterino@users.noreply.github.com>
2018-06-03 09:59:27 +0000

    removed header

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,15 +6,12 @@ import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { HomeComponent } from './home/home.component';
 import { InstructionsComponent } from './instructions/instructions.component';
 import { DomainComponent } from './domain/domain.component';
 import { CodeComponent } from './code/code.component';
 import { AnnotateComponent } from './annotate/annotate.component';
-import {DropdownDirective} from './shared/dropdown.directive';
-import { AnnotationsComponent } from './header/annotations/annotations.component';
 import { CodeAnnotationComponent } from './annotate/code-annotation/code-annotation.component';
 import { RequirementComponent } from './annotate/requirement/requirement.component';
 import { RequirementsComponent } from './domain/requirements/requirements.component';
@@ -42,13 +39,10 @@ const appRoutes: Routes = [
     AppComponent,
     UserComponent,
     HomeComponent,
-    HeaderComponent,
     InstructionsComponent,
     DomainComponent,
     CodeComponent,
     AnnotateComponent,
-    DropdownDirective,
-    AnnotationsComponent,
     CodeAnnotationComponent,
     RequirementComponent,
     RequirementsComponent,
